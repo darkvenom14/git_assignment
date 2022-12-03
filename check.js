aca9849d769dcaa4bfe02a58989830bdf82ce4c3
darkvenom14 <blal22550@gamil.com>
2022-12-03 22:24:25 +0530

    solve my code

---
+++ check.js
@@ -0,0 +1,16 @@
+//prime 
+let num=13
+let count=0;
+for( i=1 ; i<num ; i++){
+    if(num%i==0){
+        count++
+    }
+}(count==2) ? console.log("Yes")  : 
+ console.log("No")
+//plaindrome
+let num1="";
+for(let i=str.length ; i>=0 ; i--){
+    num1+=str1[i];
+     
+}(num1==str1) ? console.log("Yes"):
+       console.log ("No")   
